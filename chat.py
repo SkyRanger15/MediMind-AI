b91a8e3da8f817f5fcc83199ec5618206bfcae0c
Soumyajit <143792586+SkyRanger15@users.noreply.github.com>
2025-02-23 15:50:42 +0530

    Updated expired model

--- chat.py
+++ chat.py
@@ -27,7 +27,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-pro-exp-0827",
+  model_name="gemini-1.5-pro",
   generation_config=generation_config,
   safety_settings={
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
